1c27ee29dca743a23482161198ec1eeada2d9cd9
BennAjax <bennkeys1@gmail.com>
2021-05-30 11:55:22 +0100

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,191 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="bg-teal antialiased">
+    <header class="flex justify-between items-end bg-gray-900 px-4 py-3">
+      <div>
+        <svg
+          class="h-7 w-auto"
+          viewBox="0 0 185 32"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M60.064 27h3.768l3.144-11.088L70.144 27h3.768l4.704-16.8h-3.48L71.92 22.656 68.392 10.2H65.56l-3.504 12.456L58.84 10.2h-3.48L60.064 27zm24.61.336c3.529 0 6.36-2.76 6.36-6.336 0-3.576-2.831-6.336-6.36-6.336-3.527 0-6.335 2.76-6.335 6.336 0 3.576 2.808 6.336 6.336 6.336zm0-3.024c-1.823 0-3.24-1.368-3.24-3.312 0-1.944 1.417-3.312 3.24-3.312 1.849 0 3.265 1.368 3.265 3.312 0 1.944-1.416 3.312-3.264 3.312zm11.635-7.248V15h-3.096v12h3.096v-5.736c0-2.52 2.04-3.24 3.648-3.048V14.76c-1.512 0-3.024.672-3.648 2.304zM112.832 27l-4.968-6.072L112.688 15h-3.696l-4.128 5.28V10.2h-3.096V27h3.096v-5.448L109.232 27h3.6z"
+            fill="#fff"
+          />
+          <path
+            d="M120.089 27.336c2.352 0 4.392-1.248 5.424-3.12l-2.688-1.536c-.48.984-1.512 1.584-2.76 1.584-1.848 0-3.216-1.368-3.216-3.264 0-1.92 1.368-3.288 3.216-3.288 1.224 0 2.256.624 2.736 1.608l2.664-1.56c-.984-1.848-3.024-3.096-5.376-3.096-3.648 0-6.336 2.76-6.336 6.336 0 3.576 2.688 6.336 6.336 6.336zM136.083 15v1.416c-.864-1.08-2.16-1.752-3.912-1.752-3.192 0-5.832 2.76-5.832 6.336 0 3.576 2.64 6.336 5.832 6.336 1.752 0 3.048-.672 3.912-1.752V27h3.096V15h-3.096zm-3.336 9.384c-1.896 0-3.312-1.368-3.312-3.384s1.416-3.384 3.312-3.384c1.92 0 3.336 1.368 3.336 3.384s-1.416 3.384-3.336 3.384zm16.103-6.408V15h-2.712v-3.36l-3.096.936V15h-2.088v2.976h2.088v4.992c0 3.24 1.464 4.512 5.808 4.032v-2.808c-1.776.096-2.712.072-2.712-1.224v-4.992h2.712zm3.719-4.416c1.056 0 1.92-.864 1.92-1.896s-.864-1.92-1.92-1.92c-1.032 0-1.896.888-1.896 1.92s.864 1.896 1.896 1.896zM151.033 27h3.096V15h-3.096v12zm11.642.336c3.528 0 6.36-2.76 6.36-6.336 0-3.576-2.832-6.336-6.36-6.336-3.528 0-6.336 2.76-6.336 6.336 0 3.576 2.808 6.336 6.336 6.336zm0-3.024c-1.824 0-3.24-1.368-3.24-3.312 0-1.944 1.416-3.312 3.24-3.312 1.848 0 3.264 1.368 3.264 3.312 0 1.944-1.416 3.312-3.264 3.312zm15.21-9.648c-1.608 0-2.856.6-3.576 1.68V15h-3.096v12h3.096v-6.48c0-2.088 1.128-2.976 2.64-2.976 1.392 0 2.376.84 2.376 2.472V27h3.096v-7.368c0-3.192-1.992-4.968-4.536-4.968z"
+            fill="#A3BFFA"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M42.911 11.604L35.213 7.16v20.645h9v2h-44v-2h4v-12.72l-3.728.933L0 14.078l21.09-5.273h3.122a9.551 9.551 0 00-.68 2.559l-.483 3.975 5.164-2.982v15.448h5V7.161l-7.696 4.444a7.502 7.502 0 012.565-4.8h-4.12a7.489 7.489 0 016.646-2.972L25.017.603a7.488 7.488 0 016.696.403c1.039.6 1.88 1.41 2.5 2.347a7.461 7.461 0 012.5-2.347 7.49 7.49 0 016.698-.402l-5.593 3.229a7.488 7.488 0 016.646 2.973h-4.12a7.498 7.498 0 012.567 4.798zM24.213 27.805v-10h-6v10h6zm-11-8a2 2 0 11-4 0 2 2 0 014 0z"
+            fill="#A3BFFA"
+          />
+        </svg>
+      </div>
+      <div class="flex">
+        <button>
+          <svg
+            class="h-6 w-6 fill-current text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </div>
+    </header>
+    <section class="flex justify-between bg-gray-800 px-4 py-2">
+      <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg
+            class="w-6 h-6 fill-current text-gray-500"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M14.32 12.906l1.096 1.096c.412-.023.83.123 1.145.437l3 3a1.5 1.5 0 01-2.122 2.122l-3-3a1.497 1.497 0 01-.437-1.145l-1.096-1.096a8 8 0 111.414-1.414zM8 14A6 6 0 108 2a6 6 0 000 12z"
+              fill="#A0AEC0"
+            />
+          </svg>
+        </div>
+        <input
+          class="
+            bg-gray-900
+            focus:outline-none
+            focus:bg-white
+            focus:text-gray-900
+            text-white
+            rounded-lg
+            pl-10
+            pr-4
+            pt-1.5
+            pb-2
+          "
+          placeholder="Search by keywords"
+        />
+      </div>
+
+      <button
+        class="
+          inline-flex
+          items-center
+          bg-gray-700
+          rounded-lg
+          shadow
+          pl-3
+          pr-4
+          hover:bg-gray-600
+          focus:outline-none
+          focus:text-gray-900
+        "
+      >
+        <svg
+          class="fill-current text-gray-400"
+          width="18"
+          height="14"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M0 1a1 1 0 011-1h16a1 1 0 110 2H1a1 1 0 01-1-1zm3 6a1 1 0 011-1h10a1 1 0 110 2H4a1 1 0 01-1-1zm4 5a1 1 0 100 2h4a1 1 0 100-2H7z"
+          />
+        </svg>
+        <span class="ml-1 text-white font-medium">Filters</span>
+      </button>
+    </section>
+    <main class="px-3 py-6 space-y-2">
+      <h3 class="text-gray-900 text-xl">Los Angeles</h3>
+      <p class="text-gray-600 leading-5">
+        Live like the stars in these luxurious Southern California estates
+      </p>
+      <div>
+        <div class="mt-6" v-for="i in 5" :key="i">
+          <div class="relative pb-2/3 rounded-lg shadow-md">
+            <img
+              class="absolute h-full w-full object-cover rounded-lg shadow-md"
+              src="https://images.unsplash.com/photo-1512917774080-9991f1c4c750?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+              alt="house"
+            />
+          </div>
+          <div class="relative px-4 -mt-16">
+            <div class="bg-white rounded-lg px-4 py-4 shadow-lg">
+              <div class="flex items-center">
+                <span
+                  class="
+                    inline-block
+                    bg-green-200
+                    text-green-800
+                    rounded-full
+                    px-2
+                    py-px
+                    font-semibold
+                    uppercase
+                    tracking-wide
+                    text-xs
+                  "
+                  >Plus</span
+                >
+                <div
+                  class="
+                    ml-2
+                    text-xs text-gray-600
+                    font-semibold
+                    uppercase
+                    tracking-wide
+                  "
+                >
+                  3 Beds &bull; 2 Baths
+                </div>
+              </div>
+              <h4 class="mt-1 text-gray-900 font-semibold text-lg">
+                Modern home in city center
+              </h4>
+              <div class="mt-1">
+                <span class="text-gray-900">$1,400</span>
+                <span class="ml-1 text-sm text-gray-600">/wk</span>
+              </div>
+              <div class="mt-2 flex items-center text-sm text-gray-500">
+                <svg
+                  v-for="i in 5"
+                  :key="i"
+                  class="h-4 w-4 fill-current text-green-500"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                  />
+                </svg>
+                <span class="ml-2">34 reviews</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
